9cd93f734b882e9617991d64dbff84d26e71955a
贾航宇 <1092945018@qq.com>
2019-11-27 13:35:59 +0800

    第一个javaGit项目添加到Git

---
+++ git-test/src/com/jhy/test.java
@@ -0,0 +1,12 @@
+package com.jhy;
+
+/**
+ * @Date:2019/11/27 13:28
+ * @Author:贾航宇
+ * @注解:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
